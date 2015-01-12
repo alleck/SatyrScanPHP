59a79d7ab7ec2aadff3106c91ca5a72cbfbfc786
alleck <kaleb.lewis92@gmail.com>
2015-01-12 00:45:43 -0600

    Added first working steps and code

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ SatyrScanPHP
 ============
 SatyrScanPHP is a PHP based spam, hack, and virus scanner.
 
-It is the child of BASH SatyrScan project. Which can be found on GitHub https://github.com/alleck/SatyrScan/tree/master/Current-Version 
+It is the child of BASH SatyrScan project. Which can be found on GitHub https://github.com/alleck/SatyrScan/tree/master/Current-Version
+
+The steps are simple.
+
+1: Download the zip file. https://github.com/alleck/SatyrScanPHP/archive/master.zip
+2: Unzip the file.
+3: Make sure that the <code>scan.php</code> and the <code>Virusdb.txt</code> are located in the site's root.
+4: Visit <code>http://URL/scan.php</code> in a browser, and it will tell you what files are compromised.
---
+++ scan.php
@@ -0,0 +1,48 @@
+<?php
+
+//Initiate the file array
+$fileArray = array();
+
+$viruses = file('Virusdb.txt');
+
+foreach (new RecursiveIteratorIterator (new RecursiveDirectoryIterator ('.')) as $x)
+{
+    // Add each line that is not "." or ".." to the file array
+    if (strcmp($x->getFilename(), ".") !== 0 and strcmp($x->getFilename(), "..") !== 0) {
+
+        array_push($fileArray, $x->getPathname());
+    }
+
+}
+
+// TESTING - array outputs
+echo "<pre>";
+// TESTING - array outputs
+//print_r($viruses);
+// TESTING - array outputs
+echo "</pre>";
+
+// Look through all of the files in the file array for the virus keyword(s)
+foreach ($fileArray as $files){
+
+    // Exclude the Virusdb.txt from the scan results
+    if (strcmp($files, ".\Virusdb.txt") !== 0 and strcmp($files, ".\Virusdb.txt") !== 0)
+    {
+        $file = file_get_contents($files);
+        foreach ($viruses as $virus)
+        {
+            if(strpos($file, $virus))
+            {
+                echo $files . "<br />";
+            }
+        }
+    }
+}
+
+// Get the number of fileas that will be scanned
+$numberFiles = count($fileArray);
+
+echo $numberFiles . " Files Scanned";
+
+
+?>
